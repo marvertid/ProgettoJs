d3900e59ffb325aa8441fc268999d65381e9b78d
marvertid <marvertid@proton.me>
2023-12-16 00:59:06 +0100

    prob changes

--- home/script.js
+++ home/script.js
@@ -14,22 +14,18 @@ h1.onmouseover = async function() {
         active = true;
         for (let i = 0; i < 6; i++) {
             titleText[i].classList.add("flip");
-                random = 1 + Math.floor(8 * Math.random());
+                random = 1 + Math.floor(4 * Math.random());
                 console.log(random);
                 await delay(285);
                 switch(random) {
                     case 1:
                     case 2:
-                    case 3:
                         titleText[i].style.backgroundColor = "#3a3a3c";
                         break;
-                    case 4:
-                    case 5:
-                    case 6:
+                    case 3:
                         titleText[i].style.backgroundColor = "#b59f3b";
                         break;
-                    case 7:
-                    case 8:
+                    case 4:
                         titleText[i].style.backgroundColor = "#538d4e";
                 }
             await delay(15);
