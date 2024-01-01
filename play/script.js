3fcb401a37c41b8ecae2f9933dbd3f3505bdbf18
marvertid <marvertid@proton.me>
2024-01-01 19:21:58 +0100

    game mechanics

--- play/script.js
+++ play/script.js
@@ -1,44 +1,102 @@
-let word;
-let test = document.getElementById("test");
-let i = 0;
+import { engDictionary, itDictionary } from "../wordsDB/dictionary.js";
 
-/*window.addEventListener(
-    "keydown",
-    (event) => {
-        switch (event.code) {
-        case "ArrowDown":
-          // Do something for "down arrow" key press.
-          break;
-        case "ArrowUp":
-          // Do something for "up arrow" key press.
-          break;
-        case "ArrowLeft":
-          // Do something for "left arrow" key press.
-          break;
-        case "ArrowRight":
-          // Do something for "right arrow" key press.
-          break;
-        case "Enter":
-          // Do something for "enter" or "return" key press.
-          break;
-        case " ":
-          // Do something for "space" key press.
-          break;
-        case "Escape":
-          // Do something for "esc" key press.
-          break;
-        case "Backspace":
-            test.textContent = word.substr(i);
-          break;
-        default:
-          test.textContent = word += event.key;
-          i++;
+var test = document.getElementById("test");
+let tableTd = document.getElementsByTagName("td");
+var wordTest = document.getElementById("word");
+var language = document.documentElement.lang;
+let i;
+let j;
+let word = "";
+
+i = 0;
+j = 0;
+word = pickWord();
+
+//wordTest.textContent += word;
+
+document.body.onkeydown = (e) => {
+  const key = e.key;
+
+  if (key === "Backspace" && i > 0) {
+    let temp = test.textContent;
+    temp = temp.slice(0, -1);
+    test.textContent = temp;
+    tableTd[i - 1 + j].textContent = " ";
+    i--;
+  } else if (key === "Enter") {
+    if (checkWord(test.textContent)) {
+
+      if (test.textContent === word) {
+        alert("you won");
+        location.reload()
+
+      } else {
+
+        checkForLetter(word, tableTd, j);
+        
+        j += 5;
+        i = 0;
+        test.textContent = "";
       }
+    } 
+  } else if (key == "F1") {
+    test.textContent = "";
+    i = 0;
+    j = 0;
+  } else if (isLetter(key)) {
+      if (i < 5) {
+        test.textContent += key.toLowerCase();
+        tableTd[i + j].textContent = key;
+        i++;
+      }
+  }
+};
 
-      // Cancel the default action to avoid it being handled twice
-      event.preventDefault();
-    },
-    true,
-);
+function isLetter(key) {
+  if (key.length == 1 && ((key >= "a" && key <= "z") || (key >= "A" && key <= "Z"))) {
+    return true;
+  } 
+  return false;
+}
+
+function checkWord(word) {
+  if (language == "it") {
+    return itDictionary.includes(word);
+  } else if (language == "en")
+  {
+    return engDictionary.includes(word);
+  }
+}
+
+function pickWord() {
+  let randomWord;
+  if (language === "it") {
+    randomWord = itDictionary[Math.floor(Math.random() * itDictionary.length)];
+  } else if (language === "en") {
+    randomWord = engDictionary[Math.floor(Math.random() * engDictionary.length)];
+  }
+
+  return randomWord;
+}
 
 
+function checkForLetter(word, tableTd, j) {
+  let temp = test.textContent.split("");
+  let wordTemp = word;
+  let x;
+  for(let i = 0; i < temp.length; i++) {
+    x = wordTemp.indexOf(temp[i]);
+    if (i == word.indexOf(temp[i])) {
+      tableTd[i + j].style.background = "green";
+      wordTemp = wordTemp.replace(temp[i], "");
+    }
+  }
+
+  for(let i = 0; i < temp.length; i++) {
+    x = wordTemp.indexOf(temp[i]);
+    if (x > -1) {
+      tableTd[i + j].style.background = "orange";
+      wordTemp = wordTemp.replace(temp[i], "");
+    }
+  }
+}
